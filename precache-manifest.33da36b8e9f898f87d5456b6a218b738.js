4ccf3e1fe4359661358504930a5e9e9acfbc1ab2
Yurs Chabarenko <yurschabarenko@Yurss-MacBook-Pro.local>
2021-03-22 12:36:09 +0200

    Updates

---
+++ precache-manifest.33da36b8e9f898f87d5456b6a218b738.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7d5922a81061c989795caed96addad",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b01a1edbdc7bf5b39606",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b01a1edbdc7bf5b39606",
+    "url": "./static/js/main.9c598966.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
